d8f46fd270bd66342227e659ce2632679a0774ad
daran6255 <darandharani6255@gmail.com>
2025-05-29 16:07:18 +0530

    minor changes

--- backend/app/api/static_routes.py
+++ backend/app/api/static_routes.py
@@ -10,7 +10,7 @@ def serve_pdf(filename):
 
 @static_bp.route("/static/images/<path:filename>")
 def serve_image(filename):
-    image_dir = os.path.join(current_app.root_path, "static", "uploads", "images")
+    image_dir = os.path.join(current_app.root_path, "static", "images")
     print("Looking for image at:", image_dir)
     return send_from_directory(image_dir, filename)
 
